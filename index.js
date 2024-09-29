7634307457b2de0fbe9533c7fee9eadf2810bdf7
who-icyy <sonusid1325@gmail.com>
2024-09-29 19:02:02 +0530

    Initial

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
+const token = process.env.TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
